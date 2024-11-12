3e8f36c93ec6ff42240af740e8152d64cf64eda8
Asfor <cbb111213@nptu.edu.tw>
2024-11-12 12:35:21 +0800

    Cancel after clicking rightPanel

--- G07/src/main/java/staffEditor/MouseButton.java
+++ G07/src/main/java/staffEditor/MouseButton.java
@@ -1,18 +1,41 @@
 package staffEditor;
 
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 public class MouseButton extends IconButton{
+	
+	private JPanel rightPanel;
 
-	public MouseButton(Toolbar p) 
+	public MouseButton(Toolbar p, JPanel rightPanel) 
 	{
 		super(p);
+		this.rightPanel = rightPanel; 
         imageURL   = cldr.getResource("images/direct-selection.png");
         icon = new ImageIcon(imageURL);
         this.setIcon(icon);
         this.setToolTipText("滑鼠");
         
-        
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                disableRightButtons();  
+            }
+        });
 	}
-
+		
+	private void disableRightButtons() 
+	{		
+		for (Component btn : rightPanel.getComponents()) {
+            if (btn instanceof JButton) {
+                ((JButton) btn).setEnabled(false);  
+            }
+        }
+    }
+	
 }
