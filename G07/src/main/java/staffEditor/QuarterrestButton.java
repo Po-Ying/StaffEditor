4781769eb507dc33041f51215407f0d1e496a4bd
Asfor <cbb111213@nptu.edu.tw>
2024-11-14 18:58:40 +0800

    add

---
+++ G07/src/main/java/staffEditor/QuarterrestButton.java
@@ -0,0 +1,22 @@
+package staffEditor;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+public class QuarterrestButton extends IconButton {
+
+	public QuarterrestButton(Toolbar p) 
+	{
+		super(p);
+        imageURL   = cldr.getResource("images/quarter-note-rest.png");
+        icon = new ImageIcon(imageURL);
+        this.setIcon(icon);
+
+        this.setToolTipText("儲存檔案");
+	}
+}
